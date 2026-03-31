030d50ffa446a4c665192fc36d9f0850b2c6ac88
Sarowar786 <sarowar2287@gmail.com>
2026-03-31 16:06:18 +0600

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "cdn.dummyjson.com" },
+      { protocol: "https", hostname: "i.pravatar.cc" },
+    ],
+  },
 };
 
 export default nextConfig;
